008c2a9b2be0832abd14bf849fdf6b903560302c
minju3212 <smj3mjs@gmail.com>
2025-05-10 21:55:42 +0900

    299일수업

--- src/main/java/com/basic/myspringboot/MySpringBootAppApplication.java
+++ src/main/java/com/basic/myspringboot/MySpringBootAppApplication.java
@@ -1,13 +1,23 @@
 package com.basic.myspringboot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MySpringBootAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MySpringBootAppApplication.class, args);
+		//SpringApplication.run(MySpringBootAppApplication.class, args);
+		SpringApplication application = new SpringApplication(MySpringBootAppApplication.class);
+		//기본적으로 WebApplicationType은 웹어플리케이션이다.
+		application.setWebApplicationType(WebApplicationType.SERVLET);
+		application.run(args);
 	}
 
+	@Bean
+	public String hello(){
+		return "hello springboot";
+	}
 }
